24fa3b41ea4ada28f14db5b0bb810ca43ac7da3f
RafaelNoronhaSov <rafaelnoronhadeoliveirasoveral@gmail.com>
2025-02-01 13:59:45 -0300

    feat: added cli11 lib to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "cli11/CLI11.hpp"
 
-int main() {
-	std::cout << "hello" << std:: endl;
-	return 0;
+int main(int argc, char** argv) {
+    CLI::App app{"Welcome to Gnossia"};
+    argv = app.ensure_utf8(argv);
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
 }
